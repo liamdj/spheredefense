1f7678ff7a0a931e202d24c06afea7a9169b4c34
Liam Johansson <liamdj@princeton.edu>
2021-04-28 13:43:07 -0400

    Isosahedron board geometry

--- scripts/objects/board.js
+++ scripts/objects/board.js
@@ -1,11 +1,17 @@
 // The board
 
 let getObj = () => {
-  const sphereGeo = new THREE.SphereGeometry(settings.WORLD_RADIUS);
-  const greenMat = new THREE.MeshPhongMaterial({ color: 0x038f08 });
-  const board = new THREE.Mesh(sphereGeo, greenMat);
-  board.position.set(0, 0, 0);
-  board.timeStep = (time) => {};
-  return board;
+    const boardGeo = new THREE.IcosahedronGeometry(settings.WORLD_RADIUS, 2);
+    const boardMat = new THREE.MeshLambertMaterial({ color: 0x34c29a });
+    const mesh = new THREE.Mesh(boardGeo, boardMat);
+
+    const edgeGeo = new THREE.WireframeGeometry(boardGeo);
+    const lineMat = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 2 });
+    const edges = new THREE.LineSegments(edgeGeo, lineMat);
+
+    mesh.position.set(0, 0, 0);
+    mesh.timeStep = (time) => { };
+    mesh.add(edges)
+    return mesh;
 };
 export const board = getObj();
