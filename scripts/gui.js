6de13f62b6454400c5166d7e6ca01ac229d9b02c
Liam Johansson <liamdj@princeton.edu>
2021-05-07 16:28:17 -0400

    Change turret firing and include bullets

--- scripts/gui.js
+++ scripts/gui.js
@@ -1,44 +1,44 @@
 var gui = new dat.GUI();
 
 var options = {
-  togglePhase: function () {
-    stats.phase = stats.phase == "build" ? "flight" : "build";
-  },
+    togglePhase: function () {
+        stats.phase = stats.phase == "build" ? "flight" : "build";
+    },
 };
 
 gui
-  .add(settings, "SPAWN_FREQUENCY")
-  .min(0)
-  .max(1)
-  .step(0.001)
-  .name("Spawn Rate")
-  .listen();
+    .add(settings, "SPAWN_FREQUENCY")
+    .min(0)
+    .max(0.05)
+    .step(0.001)
+    .name("Spawn Rate")
+    .listen();
 gui
-  .add(settings, "MAX_TURRETS")
-  .min(0)
-  .max(20)
-  .step(1)
-  .name("Max Turrets")
-  .listen();
+    .add(settings, "MAX_TURRETS")
+    .min(0)
+    .max(30)
+    .step(1)
+    .name("Max Turrets")
+    .listen();
 gui
-  .add(settings, "ENEMY_SPEED")
-  .min(0)
-  .max(10)
-  .step(0.05)
-  .name("Enemy Speed")
-  .listen();
+    .add(settings, "ENEMY_SPEED")
+    .min(0)
+    .max(5)
+    .step(0.05)
+    .name("Enemy Speed")
+    .listen();
 gui
-  .add(settings, "TURRET_RANGE")
-  .min(0)
-  .max(200)
-  .step(1)
-  .name("Turret Range")
-  .listen();
+    .add(settings, "TURRET_RANGE")
+    .min(0)
+    .max(1000)
+    .step(10)
+    .name("Turret Range")
+    .listen();
 gui
-  .add(settings, "TURRET_DAMAGE")
-  .min(0)
-  .max(10)
-  .step(0.1)
-  .name("Turret Damage")
-  .listen();
+    .add(settings, "TURRET_DAMAGE")
+    .min(0)
+    .max(100)
+    .step(1)
+    .name("Turret Damage")
+    .listen();
 gui.add(options, "togglePhase").name("Toggle Phase");
--- scripts/utils.js
+++ scripts/utils.js
@@ -2,75 +2,77 @@ import { Explosion } from "./objects/particles.js";
 
 const effects = [];
 
-export const handleCollisions = (objects, scene, score, time, blobMeshes) => {
-  const dist = (pos1, pos2) =>
-    Math.sqrt(
-      Math.pow(pos1.x - pos2.x, 2) +
-        Math.pow(pos1.y - pos2.y, 2) +
-        Math.pow(pos1.z - pos2.z, 2)
-    );
-  objects.forEach((object, index) => {
-    if (object.type) {
-      // turrets shoot at all things in range
-      if (object.type == "TURRET") {
-        objects.forEach((oobject) => {
-          if (
-            oobject.type &&
-            oobject.type == "TROOP" &&
-            dist(object.mesh.position, oobject.mesh.position) < object.range
-          ) {
-            oobject.health -= object.damage;
-            const shrinkFactor = 1 - object.damage / oobject.maxHealth;
-            oobject.mesh.geometry.scale(
-              shrinkFactor,
-              shrinkFactor,
-              shrinkFactor
-            );
-          }
-        });
-      }
-
-      // tower takes damage from all nearby troops
-      if (object.type == "TOWER") {
-        objects.forEach((oobject) => {
-          if (oobject.type && oobject.type == "TROOP") {
-            if (
-              dist(object.mesh.position, oobject.mesh.position) < oobject.range
-            ) {
-              object.health -= oobject.damage;
-              if (object.health < 0) {
-                object.isGone = true;
-              }
+export const handleCollisions = (objects, scene, score, time, blobMeshes, idToEntity) => {
+    // const dist = (pos1, pos2) =>
+    //     Math.sqrt(
+    //         Math.pow(pos1.x - pos2.x, 2) +
+    //         Math.pow(pos1.y - pos2.y, 2) +
+    //         Math.pow(pos1.z - pos2.z, 2)
+    //     );
+    objects.forEach((object, index) => {
+        if (object.type) {
+            // turrets shoot at something in range
+            if (object.type == "TURRET") {
+                let target = null;
+                let targetDist = object.range;
+                blobMeshes.forEach((mesh) => {
+                    const dist = object.mesh.position.distanceTo(mesh.position);
+                    if (dist < targetDist) {
+                        console.log(dist);
+                        target = mesh;
+                        targetDist = dist;
+                    }
+                });
+                if (target != null) {
+                    const bullet = object.attemptFireBullet(target.position, time);
+                    if (bullet) {
+                        objects.push(bullet);
+                        scene.add(bullet.mesh);
+                        const blob = idToEntity.get(target.id);
+                        blob.health -= object.damage;
+                        const size = 0.25 + 0.75 * blob.health / blob.maxHealth;
+                        target.scale.set(size, size, size);
+                    }
+                }
             }
-          }
-        });
-      }
+        }
 
-      // remove any objects with no health or too far away
-      if (object.isGone) {
-        scene.remove(object.mesh);
-        objects.splice(index, 1);
-        // if we removed the tower, game over
+        // tower takes damage from all nearby troops
         if (object.type == "TOWER") {
-          objects.forEach((obj) => {
-            scene.remove(obj);
-          });
-          alert("Game Over...");
-          stats.gameover = true;
+            blobMeshes.forEach((mesh) => {
+                const blob = idToEntity.get(mesh.id);
+                if (
+                    object.mesh.position.distanceTo(mesh.position) < blob.range
+                ) {
+                    object.health -= blob.damage;
+                }
+            });
         }
-        // if we remove a troop, increment score and add visual effect
-        if (object.type == "TROOP") {
-          blobMeshes.splice(
-            blobMeshes.findIndex((e) => e.id == object.mesh.id),
-            1
-          );
-          stats.score += 1;
-          document.getElementById("score").innerHTML = stats.score;
-          const explosion = new Explosion(object.mesh.position, time);
-          objects.push(explosion);
-          scene.add(explosion.mesh);
+
+        // remove any objects with no health or too far away
+        if (object.isGone) {
+            scene.remove(object.mesh);
+            objects.splice(index, 1);
+            // if we removed the tower, game over
+            if (object.type == "TOWER") {
+                objects.forEach((obj) => {
+                    scene.remove(obj);
+                });
+                alert("Game Over...");
+                stats.gameover = true;
+            }
+            // if we remove a troop, increment score and add visual effect
+            if (object.type == "TROOP") {
+                blobMeshes.splice(
+                    blobMeshes.findIndex((e) => e.id == object.mesh.id),
+                    1
+                );
+                stats.score += 1;
+                document.getElementById("score").innerHTML = stats.score;
+                const explosion = new Explosion(object.mesh.position, time);
+                objects.push(explosion);
+                scene.add(explosion.mesh);
+            }
         }
-      }
-    }
-  });
+    });
 };
