1511465c04bf600c16e03c8bb2d7f46de352a318
nathanalam <nathanalam99@gmail.com>
2021-05-12 15:57:57 -0400

    use crosshairs on github

--- scripts/objects/Crosshairs.js
+++ scripts/objects/Crosshairs.js
@@ -1,6 +1,6 @@
 export class Crosshairs {
   constructor() {
-    const map = new THREE.TextureLoader().load("../../images/crosshairs.png");
+    const map = new THREE.TextureLoader().load(`${siteurl}/images/crosshairs.png`);
     const material = new THREE.SpriteMaterial({ map: map });
     this.sprite = new THREE.Sprite(material);
     this.sprite.scale.set(0.2, 0.2, 1);
